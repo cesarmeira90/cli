1b4afa1018c1726e72d02baee913d034e74f7570
João Pereira <joaopereira@criticalmanufacturing.com>
2022-02-18 14:51:43 +0000

    test: pack html package

--- tests/Specs/Pack.cs
+++ tests/Specs/Pack.cs
@@ -1,10 +1,16 @@
 ﻿using Cmf.Common.Cli.Commands;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
 using System.CommandLine;
 using System.CommandLine.Invocation;
 using System.CommandLine.IO;
 using System.IO;
 using System.IO.Abstractions;
+using System.IO.Abstractions.TestingHelpers;
+using System.IO.Compression;
+using System.Linq;
+using tests.Objects;
 
 namespace tests.Specs
 {
@@ -131,6 +137,41 @@ namespace tests.Specs
             Assert.AreEqual("Package", _outputDir);
             Assert.IsNotNull(_force);
             Assert.IsFalse(_force ?? true);
+        }
+
+        [TestMethod]
+        public void HTML()
+        {
+            var fileSystem = MockPackage.Html;
+
+            var packCommand = new PackCommand(fileSystem);
+            packCommand.Execute(fileSystem.DirectoryInfo.FromDirectoryName("."), fileSystem.DirectoryInfo.FromDirectoryName("output"), false);
+
+            IEnumerable<IFileInfo> assembledFiles = fileSystem.DirectoryInfo.FromDirectoryName("output").EnumerateFiles("Cmf.Custom.HTML.1.1.0.zip");
+            Assert.AreEqual(1, assembledFiles.Count());
+
+            using (Stream zipToOpen = fileSystem.FileStream.Create(assembledFiles.First().FullName, FileMode.Open))
+            {
+                using (ZipArchive zip = new(zipToOpen, ZipArchiveMode.Read))
+                {
+                    // these tuples allow us to rewrite entry paths
+                    var entriesToExtract = new List<Tuple<ZipArchiveEntry, string>>();
+                    entriesToExtract.AddRange(zip.Entries.Select(selector: entry => new Tuple<ZipArchiveEntry, string>(entry, entry.FullName)));
+
+                    List<string> expectedFiles = new()
+                    {
+                        "config.json",
+                        "manifest.xml",
+                        @"node_modules\customization.package\package.json",
+                        @"node_modules\customization.package\customization.common.js"
+                    };
+                    Assert.AreEqual(expectedFiles.Count, entriesToExtract.Count);
+                    foreach (var expectedFile in expectedFiles)
+                    {
+                        Assert.IsNotNull(entriesToExtract.FirstOrDefault(x => x.Item2.Equals(expectedFile)));
+                    }
+                }
+            }
         }
     }
 }
---
+++ tests/Specs/PackageTypeHandler.cs
@@ -0,0 +1,49 @@
+﻿using Cmf.Common.Cli.Constants;
+using Cmf.Common.Cli.Factories;
+using Cmf.Common.Cli.Handlers;
+using Cmf.Common.Cli.Interfaces;
+using Cmf.Common.Cli.Objects;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.Abstractions.TestingHelpers;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using tests.Objects;
+
+namespace tests.Specs
+{
+    [TestClass]
+    public class PackageTypeHandler
+    {
+        [TestMethod]
+        public void GetContentToPack_WithNonExistentIgnoreFiles()
+        {
+            var fileSystem = MockPackage.Html;
+
+            ExecutionContext.Initialize(fileSystem);
+
+            using (StringWriter standardOutput = new())
+            {
+                Console.SetOut(standardOutput);
+                string exceptionMessage = string.Empty;
+                try
+                {
+                    var cmfPackage = fileSystem.FileInfo.FromFileName(CliConstants.CmfPackageFileName);
+                    var packageTypeHandler = PackageTypeFactory.GetPackageTypeHandler(cmfPackage) as PresentationPackageTypeHandler;
+
+                    packageTypeHandler.GetContentToPack(fileSystem.DirectoryInfo.FromDirectoryName("output"));
+                }
+                catch (Exception ex)
+                {
+                    exceptionMessage = ex.Message;
+                }
+
+                Assert.AreEqual(string.Empty, exceptionMessage);
+                Assert.AreEqual($"{ MockUnixSupport.Path(@"C:\src\packages\customization.common\") }.npmignore not found!", standardOutput.ToString().Trim());
+            }
+        }
+    }
+}
