cb7741c338d16a4325e3aa4b00820b66fa06d594
César Meira <cesarmeira@criticalmanufacturing.com>
2025-11-21 10:55:01 +0000

    fix: remove default deps removal from CmfPackageV1

--- core/Objects/CmfPackage/CmfPackageV1.cs
+++ core/Objects/CmfPackage/CmfPackageV1.cs
@@ -265,13 +265,8 @@ public class CmfPackageV1 : IEquatable<CmfPackageV1>
                       bool? isToSetDefaultSteps, DependencyCollection dependencies, List<Step> steps,
                       List<ContentToPack> contentToPack, List<string> xmlInjection, bool? waitForIntegrationEntries, DependencyCollection testPackages = null) : this()
     {
-
         if (dependencies != null)
         {
-            // Remove default dependencies to ignore as they are not relevant for cli operations
-            var packageIdsToIgnore = new HashSet<string>(Dependency.DefaultDependenciesToIgnore.Select(id => id.ToLowerInvariant()));
-            dependencies.RemoveAll(d => packageIdsToIgnore.Contains(d.Id.ToLowerInvariant()));
-
             // Normalize version ranges to just the upper limit
             foreach (var dep in dependencies)
             {
